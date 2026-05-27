8da26423a0e7ff752944a6e282c417aa21281264
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-05-27 14:25:54 -0300

    Initial configuration

---
+++ src/main/java/com/agile_development/tdd_activity/TddActivityApplication.java
@@ -0,0 +1,13 @@
+package com.agile_development.tdd_activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddActivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agile_development/tdd_activity/TddActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile_development.tdd_activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
