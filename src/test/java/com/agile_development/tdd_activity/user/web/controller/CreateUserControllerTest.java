f6e11749d2535f02adf6be5de437643e35aabd8c
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-05-30 17:38:50 -0300

    test: Test added to the GetUseUserCase

--- src/test/java/com/agile_development/tdd_activity/user/web/controller/CreateUserControllerTest.java
+++ src/test/java/com/agile_development/tdd_activity/user/web/controller/CreateUserControllerTest.java
@@ -1,4 +1,4 @@
-package com.agile_development.tdd_activity.user.application.controller;
+package com.agile_development.tdd_activity.user.web.controller;
 import com.agile_development.tdd_activity.user.web.controller.CreateUserController;
 import com.agile_development.tdd_activity.user.application.dto.CreateUserRequest;
 import com.agile_development.tdd_activity.user.application.dto.CreateUserResponse;
