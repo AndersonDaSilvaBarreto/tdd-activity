684bd047ea2ab6a2cb4c3c7d1b73073f5dc1b918
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-05-31 20:51:40 -0300

    refactor: Post refactored

--- src/main/java/com/agile_development/tdd_activity/post/domain/entity/Post.java
+++ src/main/java/com/agile_development/tdd_activity/post/domain/entity/Post.java
@@ -3,10 +3,7 @@ package com.agile_development.tdd_activity.post.domain.entity;
 import com.agile_development.tdd_activity.post.domain.valueobject.Content;
 import com.agile_development.tdd_activity.post.domain.valueobject.PostId;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -30,8 +27,7 @@ public class Post {
     @Column(name = "create_at", nullable = false, updatable = false)
     private Instant createAt;
 
-    private Post(Content content, UUID authorId) {
-        validate(content, authorId);
+    private Post(@NonNull Content content,@NonNull UUID authorId) {
         this.id = PostId.generate();
         this.content = content;
         this.authorId = authorId;
@@ -42,20 +38,8 @@ public class Post {
         return new Post(content, authorId);
     }
 
-    private void validate(Content content, UUID authorId) {
-        if (content == null) {
-            throw new IllegalArgumentException("Content cannot be null");
-        }
-        if (authorId == null) {
-            throw new IllegalArgumentException("AuthorId cannot be null");
-        }
-    }
-
-    public void changeContent(Content content) {
-        if (content == null) {
-            throw new IllegalArgumentException("Content cannot be null");
-        }
-        this.content = content;
+    public void changeContent(@NonNull Content newContent) {
+        this.content = newContent;
     }
 
 
