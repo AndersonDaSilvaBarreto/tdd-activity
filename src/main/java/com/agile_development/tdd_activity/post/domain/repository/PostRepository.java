30a9ebc5b512e40621ef12e17565e4ca13f30508
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-05-31 16:42:08 -0300

    feat: PostRepository interface added

---
+++ src/main/java/com/agile_development/tdd_activity/post/domain/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.agile_development.tdd_activity.post.domain.repository;
+
+import com.agile_development.tdd_activity.post.domain.entity.Post;
+import com.agile_development.tdd_activity.post.domain.valueobject.PostId;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PostRepository {
+    Post save (Post post);
+    Optional<Post> findById(PostId postId);
+    List<Post> findPostsByAuthorId(UUID authorId);
+}
--- src/main/java/com/agile_development/tdd_activity/user/infra/persistence/UserJpaRepository.java
+++ src/main/java/com/agile_development/tdd_activity/user/infra/persistence/UserJpaRepository.java
@@ -6,7 +6,6 @@ import com.agile_development.tdd_activity.user.domain.valueobject.UserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface UserJpaRepository extends JpaRepository<User, UserId> {
     Optional<User> findByEmail(Email email);
