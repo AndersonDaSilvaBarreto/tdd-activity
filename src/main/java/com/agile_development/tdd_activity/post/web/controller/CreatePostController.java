b3b7f897456e9cde16b34e81130b99325557ec89
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-05-31 18:37:34 -0300

    feat: CreatePostController added

--- src/main/java/com/agile_development/tdd_activity/post/web/controller/CreatePostController.java
+++ src/main/java/com/agile_development/tdd_activity/post/web/controller/CreatePostController.java
@@ -1,9 +1,14 @@
 package com.agile_development.tdd_activity.post.web.controller;
 
 import com.agile_development.tdd_activity.post.application.usecase.CreatePostUseCase;
+import com.agile_development.tdd_activity.post.web.dto.CreatePostRequest;
 import com.agile_development.tdd_activity.post.web.dto.PostResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CreatePostController {
     private final CreatePostUseCase useCase;
-    public ResponseEntity<PostResponse> create(
 
-    )
+    @PostMapping
+    public ResponseEntity<PostResponse> create(
+            @RequestBody @Valid CreatePostRequest request
+    ) {
+        PostResponse response = useCase.execute(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
