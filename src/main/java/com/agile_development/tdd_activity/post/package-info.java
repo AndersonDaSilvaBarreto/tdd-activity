a32b6d522b7ab4a5459c920d776f5c2097988b69
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-05-31 18:04:26 -0300

    refactor: package-info.java refactored

---
+++ src/main/java/com/agile_development/tdd_activity/post/package-info.java
@@ -0,0 +1,7 @@
+@ApplicationModule(
+        displayName = "Post Module",
+        allowedDependencies = "user :: api"
+
+)
+package com.agile_development.tdd_activity.post;
+import org.springframework.modulith.ApplicationModule;
---
+++ src/main/java/com/agile_development/tdd_activity/user/package-info.java
@@ -0,0 +1,7 @@
+@ApplicationModule(
+        displayName = "User Module",
+        allowedDependencies = {}
+)
+package com.agile_development.tdd_activity.user;
+
+import org.springframework.modulith.ApplicationModule;
