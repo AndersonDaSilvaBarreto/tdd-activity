c0918d77240422719f4e549c84070896c8f22531
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-06-01 12:19:50 -0300

    refactor: optimize post deletion by author id

--- src/main/java/com/agile_development/tdd_activity/post/infra/persistense/SpringDataPostRepository.java
+++ src/main/java/com/agile_development/tdd_activity/post/infra/persistense/SpringDataPostRepository.java
@@ -3,11 +3,18 @@ package com.agile_development.tdd_activity.post.infra.persistense;
 import com.agile_development.tdd_activity.post.domain.entity.Post;
 import com.agile_development.tdd_activity.post.domain.valueobject.PostId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface SpringDataPostRepository extends JpaRepository<Post, PostId> {
     List<Post> findPostsByAuthorId(UUID authorId);
+    @Modifying
+    @Query("""
+       delete from Post p
+       where p.authorId = :authorId
+""")
     void deleteAllByAuthorId(UUID authorId);
 }
