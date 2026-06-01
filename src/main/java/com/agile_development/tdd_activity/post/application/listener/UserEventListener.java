1c22f2603911dbde0c0218508474de9a75cd4324
Anderson Da Silva Barreto <andersondasilvabarreto.pro@gmail.com>
2026-06-01 00:34:44 -0300

    refactored: UserEventListener updated

--- src/main/java/com/agile_development/tdd_activity/post/application/listener/UserEventListener.java
+++ src/main/java/com/agile_development/tdd_activity/post/application/listener/UserEventListener.java
@@ -4,7 +4,6 @@ import com.agile_development.tdd_activity.post.domain.repository.PostRepository;
 import com.agile_development.tdd_activity.user.api.event.UserDeletedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.modulith.events.ApplicationModuleListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Component;
 public class UserEventListener {
     private final PostRepository repository;
 
-    @Async
     @ApplicationModuleListener
     public void on (UserDeletedEvent event) {
         repository.deleteAllByAuthorId(event.id());
